142c4730519724ae7a4802f5263cab7c739f48d0
lilshetty <sathwiksitharamshetty5678@gmail.com>
2023-12-10 14:50:20 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -31,6 +31,7 @@ var swiper = new Swiper(".slide-container-event", {
     },
   },
 });
+
 var swiper = new Swiper(".slide-container-event1", {
   slidesPerView: 3,
   spaceBetween: 20,
@@ -64,8 +65,6 @@ var swiper = new Swiper(".slide-container-event1", {
     },
   },
 });
-
-
 
 var swiper = new Swiper(".slide-container-event2", {
   slidesPerView: 3,
@@ -101,22 +100,16 @@ var swiper = new Swiper(".slide-container-event2", {
   },
 });
 
-
-
-  
-
 const body = document.querySelector("body"),
 nav = document.querySelector("nav"),
 searchToggle = document.querySelector(".searchToggle"),
 sidebarOpen = document.querySelector(".sidebarOpen"),
 siderbarClose = document.querySelector(".siderbarClose");
 
-
 // js code to toggle search box
   searchToggle.addEventListener("click" , () =>{
   searchToggle.classList.toggle("active");
 });
-
 
 //   js code to toggle sidebar
 sidebarOpen.addEventListener("click" , () =>{
@@ -131,5 +124,16 @@ if(!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contai
 }
 });
 
-  
+let bg = document.getElementById("bg");
+        let box = document.getElementById("box");
+        let hexafloat = document.getElementById("hexafloat");
+        let hexa = document.getElementById("hexa");
 
+        window.addEventListener('scroll', function () {
+            var value = window.scrollY;
+            bg.style.top = value * 0.5 + 'px';
+            box.style.left = -value * 0.5 + 'px';
+            hexafloat.style.top = -value * 0.15 + 'px';
+            hexa.style.top = value * 0.15 + 'px';
+            texthome.style.top = value * 1 + 'px';
+        })
